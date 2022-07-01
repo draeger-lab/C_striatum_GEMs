b3e48c1278cf8b49b492e86374c9041eb1842a2e
famosab <famke.baeuerle@gmail.com>
2022-07-01 17:54:56 +0200

    Remove SIRA2, FPRA and GCDH to eliminate EGC #1

--- scripts/man_cur_deadends.py
+++ scripts/man_cur_deadends.py
@@ -1,6 +1,5 @@
 #%%
 import cobra
-
 modelpath = '../models/Cstr_15.xml'
 model = cobra.io.read_sbml_model(modelpath)
 
@@ -27,22 +26,3 @@ mod = read.getModel()
 mod.getListOfSpecies().getElementBySId('M_spmd_c') 
 mod.getListOfReactions().getElementBySId('R_SPMS').getReversible()
 # this reaction has to be reversible according to MetaCyc and Rhea
-
-#%%
-import sqlite3
-import pandas as pd
-
-con = sqlite3.connect("../../Databases/bigg.sqlite")
-
-#%%
-cur = con.cursor()
-    
-df = pd.read_sql_query("SELECT * from model_reaction", con)
-df3 = pd.read_sql_query("SELECT * from model", con)
-
-df2 = pd.read_sql_query("SELECT * from reaction", con)
-df4 = df2.rename({'id': 'reaction_id'}, axis=1).merge(df)
-
-df3.rename({'id': 'model_id', 'bigg_id': 'bigg_model_id'}, axis=1).merge(df4)
-df5 = pd.read_sql_query("SELECT * from complex_composition", con)
-df5
