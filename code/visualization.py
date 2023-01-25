7ec9386a68c2dfc62dccde0c49c45c43d175a919
famosab <famke.baeuerle@gmail.com>
2023-01-25 17:30:53 +0100

    heatmap with code #4

--- code/visualization.py
+++ code/visualization.py
@@ -7,6 +7,7 @@ import refinegems as rg
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 from PARAMS import *
 
 #%%
@@ -15,7 +16,7 @@ mods = []
 for mod in models:
     mods.append(rg.load.load_model_cobra(mod))
 
-#%%
+#%% Venn diagrams
 def create_venn(mods, entity, perc=False): 
     intersec = {}
     for model in mods:
@@ -45,7 +46,7 @@ plt.tight_layout()
 plt.savefig('../analysis/comparison/' + name + '.png')
 
 
-# %%
+# %% barcharts
 numbers = pd.DataFrame(['reactions', 'metabolites', 'genes'])
 numbers
 #%%
@@ -57,4 +58,37 @@ numbers.set_index(0).T.plot.bar(cmap='Paired')#color={'reactions': cb[2], 'metab
 plt.legend()
 plt.xticks(rotation=45, ha='right')
 plt.yticks(np.asarray([0,500,1000,1500,2000, 2500]))
-# %%
+# %% heatmap
+
+growth = pd.read_csv('../analysis/growth_20230113_minimal_uptake.csv')[['model', 'medium', 'doubling_time [min]']]
+growth=growth.set_index(['medium', 'model']).sort_index().T.stack()
+growth.columns.name=None
+growth.index.names = (None,None)
+growth.index.name=None
+growth.index = growth.index.get_level_values(1)
+growth.replace([np.inf, -np.inf], np.nan, inplace=True)
+growth
+#%%
+plt.figure(figsize=(10,8))
+sns.heatmap(growth.T, 
+            annot=True, 
+            annot_kws={"fontsize":15},
+            vmin=20, vmax=80,
+            #cmap='crest', 
+            linewidth=.5, 
+            cbar_kws = {'orientation':'horizontal', 'label':'doubling time [min]'}
+            )
+plt.xticks(rotation=0)
+plt.yticks(rotation=0)
+plt.tick_params(
+    axis='x',          # changes apply to the x-axis
+    which='both',      # both major and minor ticks are affected
+    bottom=False,      # ticks along the bottom edge are off
+    top=False,         # ticks along the top edge are off
+    )
+plt.tick_params(
+    axis='y',          # changes apply to the x-axis
+    which='both',      # both major and minor ticks are affected
+    left=False,
+    )
+plt.savefig('../analysis/comparison/heatmap_dt.png')
